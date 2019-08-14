6b5bb8b4ce7b9ef8893a7deaf6783904de79205c
hhimanshu <harit.himanshu@yahoo.in>
2019-08-14 11:35:27 -0700

    Changed the theme, font and the name of the app

--- src/theme.js
+++ src/theme.js
@@ -3,21 +3,10 @@ import { createMuiTheme } from '@material-ui/core/styles';
 export const theme = createMuiTheme({
     palette: {
         primary: {
-            main: '#3640FF',
+            main: '#FFDE03',
         },
         secondary: {
-            main: '#FFFFFF',
-            light: '#F8F9FA'
+            main: '#0336ff',
         },
-        text: {
-            primary: 'rgba(0,0,0,1)',
-            secondary: 'rgba(0,0,0,0.6)',
-        }
     },
-    typography: {
-        useNextVariants: true,
-        h5: {
-            fontWeight: 500,
-        }
-    }
 });
